eba557d4c499359d9624382e8ca0de8f92064baf
a-a-j <johnadhira@gmail.com>
2025-08-14 10:51:38 -0600

    A few changes (sorry)
    Changed name of page title
    Some media queries for gallery & font size
    Commented out the pulse effect
    Added second font (Lato)
    Remove box shadow for stats
    Change spacing for paragraphs
    Make ACF logo smaller

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const totalElement = document.getElementById("donation-total");
   const targetAmount = 1000; // current amount raised
   const progressBar = document.getElementById("progress-bar");
-  const targetProgress = 25; // current total miles run
+  const targetProgress = 25; // percent (%) current total miles run
   let hasAnimated = false;
 
   function formatMoney(amount) {
